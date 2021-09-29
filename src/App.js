548d08f67d4e0827892481fc17836bd477e2a5db
Mubeen Qazi <mubeenqazi32@gmail.com>
2021-09-29 18:18:48 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import Fuse from "fuse.js";
+
+import data from "./data";
+
+import "./App.css";
+
+function App() {
+  const [search, setSearch] = useState("");
+  const [reports, setReports] = useState(data.reports);
+
+  useEffect(() => {
+    if (search) {
+      const fuse = new Fuse(data.reports, {
+        keys: ["name", "category", "keywords"],
+        threshold: 0.4,
+        includeScore: true,
+      });
+      setReports(fuse.search(search[0]));
+    }
+  }, [search]);
+  return (
+    <div className="App">
+      <input
+        type="text"
+        onChange={(e) => setSearch(e.target.value.split(" "))}
+      />
+
+      {reports?.map((data) => {
+        const report = search.length > 0 ? data?.item : data;
+
+        return (
+          <div key={report?.id}>
+            <h2>{report?.category}</h2>
+            <p>{report?.name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
